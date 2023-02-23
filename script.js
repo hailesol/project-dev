1a8ca98b15afed0b3f5c6e2034f353c53d0c6c41
hailesol <126162623+hailesol@users.noreply.github.com>
2023-02-23 16:13:13 +0000

    Update script.js

--- script.js
+++ script.js
@@ -24,3 +24,27 @@ function complete() {
   clearInterval(timer);
   timer = null;
 }
+var i = 0;
+var txt = 'Coming soon...';
+var speed = 100;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("coming-soon").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  } else {
+    setTimeout(function() {
+      document.getElementById("coming-soon").innerHTML = "";
+      i = 0;
+      typeWriter();
+    }, 2000); // delay before restarting the animation (in milliseconds)
+  }
+}
+
+typeWriter(); // start the animation
+setInterval(function() {
+  document.getElementById("coming-soon").innerHTML = "";
+  i = 0;
+  typeWriter();
+}, 15000); // repeat the animation every 15 seconds (in milliseconds)
