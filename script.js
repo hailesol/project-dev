cac768340958a7f8340681ebecb706686024f168
hailesol <126162623+hailesol@users.noreply.github.com>
2023-02-23 16:25:37 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const text = document.querySelector(".typewriter h1");
+const text = document.querySelector(".typewriter h1 span");
 const strText = text.textContent;
 const splitText = strText.split("");
 text.textContent = "";
@@ -24,18 +24,19 @@ function complete() {
   clearInterval(timer);
   timer = null;
 }
+
 var i = 0;
 var txt = 'Coming soon...';
 var speed = 100;
 
 function typeWriter() {
   if (i < txt.length) {
-    document.getElementById("coming-soon").innerHTML += txt.charAt(i);
+    document.querySelector(".typewriter h1 span").innerHTML += txt.charAt(i);
     i++;
     setTimeout(typeWriter, speed);
   } else {
     setTimeout(function() {
-      document.getElementById("coming-soon").innerHTML = "";
+      document.querySelector(".typewriter h1 span").innerHTML = "";
       i = 0;
       typeWriter();
     }, 2000); // delay before restarting the animation (in milliseconds)
@@ -44,7 +45,7 @@ function typeWriter() {
 
 typeWriter(); // start the animation
 setInterval(function() {
-  document.getElementById("coming-soon").innerHTML = "";
+  document.querySelector(".typewriter h1 span").innerHTML = "";
   i = 0;
   typeWriter();
 }, 15000); // repeat the animation every 15 seconds (in milliseconds)
